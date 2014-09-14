c5617f1771656680a4cc69cd0b1f1204d0c33e0e
vinivsb <vinicius@qcx.com.br>
2014-09-14 18:02:58 -0300

    web notification sample - hello world

---
+++ app/scripts/webkitnotify.js
@@ -0,0 +1,54 @@
+/*
+* Attach request permisison on load page
+* 
+*/
+window.addEventListener('load', function () {
+  Notification.requestPermission(function (status) {
+    if (Notification.permission !== status) {
+      Notification.permission = status;
+    }
+  });
+});
+
+/*
+* A sample with tags on the constructor, for group messages.
+*
+*/
+document.getElementById("sample-with-tags").addEventListener("click", function () {
+	if(window.Notification && Notification.permission == "granted"){
+		var note3 = new Notification("Hi bro.!", {tag: "tagName"});
+		var note4 = new Notification("Hi bro.! This rewrite another", {tag: "tagName"});
+	}
+});
+
+/*
+* Just a sample
+*
+*/
+document.getElementById("sample").addEventListener("click", function () {
+	if(window.Notification && Notification.permission == "granted"){
+		var note = new Notification("Hey brother!");
+		var note2 = new Notification("Hey brother! 2");
+	}
+});
+
+
+/*
+* A sample with icon on the constructor.
+*
+*/
+document.getElementById("sample-with-icon").addEventListener("click", function () {
+	if(window.Notification && Notification.permission == "granted"){
+		var note5 = new Notification("Look the awesome HTML5 icon! :P", {icon: "./images/icon.jpg"});
+	}
+});
+
+/*
+* A sample with custom body string.
+*
+*/
+document.getElementById("sample-with-custom-body").addEventListener("click", function () {
+	if(window.Notification && Notification.permission == "granted"){
+		var note6 = new Notification("With HTML5 icon and..", {icon: "./images/icon.jpg", body: "..a custom body string!"});
+	}
+});
